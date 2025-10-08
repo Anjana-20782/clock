9c494504a06d78e8a49ab7e7c1504346208ba359
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-10-08 12:12:05 +0530

    complted the clock

--- clock.js
+++ clock.js
@@ -1,12 +1,26 @@
-let hourHand=document.getElementById('hour')
-let minuteHand=document.getElementById('minute')
-let secondHand=document.getElementById('second')
+setInterval(setClock,1000)
+
+let hourHand=document.getElementById('hour');
+let minuteHand=document.getElementById('minute');
+let secondHand=document.getElementById('second');
 
 
 function setClock(){
 
-    let currentDate=new Date()
+    let currentDate=new Date();
     let secondRatio=currentDate.getSeconds() / 60;
     let minuteRatio=(secondRatio + currentDate.getMinutes()) / 60;
     let hourRatio=(minuteRatio + currentDate.getHours()) / 12;
+
+    setRotation(secondHand , secondRatio);
+    setRotation(minuteHand , minuteRatio);
+    setRotation(hourHand , hourRatio);
 }
+
+
+function setRotation(element,rotationRatio){
+
+    element.style.setProperty('--rotation',rotationRatio * 360);
+}
+
+setClock()
